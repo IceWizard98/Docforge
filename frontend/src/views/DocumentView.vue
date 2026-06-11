7da2fa62b425e815001073affd02c6c3914e837c
Luis Enriquez Aracena Cabrera <l@luise.ac>
2026-06-11 12:04:21 +0200

    fix: show full-page loading spinner in DocumentView
    Show LoadingSpinner while documentStore.loading is true and no
    document has been loaded yet.
    
    Refs: Area 10

--- frontend/src/views/DocumentView.vue
+++ frontend/src/views/DocumentView.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
+import { useDocumentStore } from '@/stores/documentStore'
 import DocumentEditor from '@/components/editor/DocumentEditor.vue'
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue'
 
 const route = useRoute()
+const documentStore = useDocumentStore()
 
 const hasId = !!route.params.id
 </script>
 
 <template>
-  <DocumentEditor v-if="hasId" />
+  <DocumentEditor v-if="hasId && documentStore.currentDocId" />
+  <LoadingSpinner v-else-if="hasId && documentStore.loading" size="lg" />
   <div v-else class="flex items-center justify-center h-screen text-foreground/40 text-sm">
     Document ID is missing
   </div>
